a037f4930fc63355c23503e929c6dcd05fd5a369
Chiara Sabato <chiarasabato@chiaras-mbp-2.wireless-1x.unc.edu>
2024-11-21 16:01:17 -0500

    Service features can be updated by logged-in user.

--- app/services.py
+++ app/services.py
@@ -17,9 +17,9 @@ openapi_tags = {
     "description": "Modify and create services",
 }
 
-# Create a new user
+# Create a new service for logged-in user
 @api.post("/create-service", tags=["services"])
-def create_marketplace(service: Services):
+def create_service(service: Services):
     try:
 
         if str(service.marketplace_id) in get_marketplaces()['message']:
@@ -42,11 +42,10 @@ def create_marketplace(service: Services):
         print("Error: ", e)
         return {"message": e}
     
-
+# Get the services owned by the logged-in user
 @api.get("/get-services", tags=["services"])
 def get_services():
     try:
-
         try:
             id = uuid.UUID(user_id()['message'])  # Convert to UUID
         except ValueError:
@@ -58,6 +57,97 @@ def get_services():
             .eq("seller", id)
             .execute()
         )
+
+        if response:
+            return {"message": f"{response}"} 
+        else:
+            return {"message": "Services could not be found"}
+    except Exception as e:
+        print("Error: ", e)
+        return {"message": e}
+    
+# Edit name of a services that the logged-in user owns
+@api.put("/update-service-name", tags=["services"])
+def update_service_name(new_name: str):
+    try:
+        try:
+            id = uuid.UUID(user_id()['message'])  # Convert to UUID
+        except ValueError:
+            raise ValueError("Invalid UUID format for user ID")
+
+        response = (
+        supabase.table("services")
+        .update({"name": new_name})
+        .eq("seller", id)
+        .execute()
+        )
+
+        if response:
+            return {"message": f"{response}"} 
+        else:
+            return {"message": "User could not be found"}
+    except Exception as e:
+        print("Error: ", e)
+        return {"message": e}
+    
+# Edit description of a services that the logged-in user owns
+@api.put("/update-service-description", tags=["services"])
+def update_service_description(new_description: str):
+    try:
+        try:
+            id = uuid.UUID(user_id()['message'])  # Convert to UUID
+        except ValueError:
+            raise ValueError("Invalid UUID format for user ID")
+
+        response = (
+        supabase.table("services")
+        .update({"description": new_description})
+        .eq("seller", id)
+        .execute()
+        )
+
+        if response:
+            return {"message": f"{response}"} 
+        else:
+            return {"message": "User could not be found"}
+    except Exception as e:
+        print("Error: ", e)
+        return {"message": e}
+    
+# Edit price of a services that the logged-in user owns
+@api.put("/update-service-price", tags=["services"])
+def update_service_price(new_price: float):
+    try:
+        try:
+            id = uuid.UUID(user_id()['message'])  # Convert to UUID
+        except ValueError:
+            raise ValueError("Invalid UUID format for user ID")
+
+        response = (
+        supabase.table("services")
+        .update({"price": new_price})
+        .eq("seller", id)
+        .execute()
+        )
+
+        if response:
+            return {"message": f"{response}"} 
+        else:
+            return {"message": "User could not be found"}
+    except Exception as e:
+        print("Error: ", e)
+        return {"message": e}
+
+# Delete service of the logged-in user
+@api.delete("/delete-service", tags=["services"])
+def delete_service(service_id: int):
+    try:
+        try:
+            id = uuid.UUID(user_id()['message'])  # Convert to UUID
+        except ValueError:
+            raise ValueError("Invalid UUID format for user ID")
+            
+        response = supabase.table('services').delete().eq('id', service_id).execute()
 
         if response:
             return {"message": f"{response}"} 
