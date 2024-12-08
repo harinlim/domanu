7d3e65f42e7204af9da326ad0d68ca9fadae9982
Harin Lim <hlim@unc.edu>
2024-12-08 13:24:48 -0500

    refactor: profile page

--- src/app/profile/bids/page.tsx
+++ src/app/profile/bids/page.tsx
@@ -1,18 +1,17 @@
 'use client'
 
-import { AppShell, AppShellMain, Title, Grid } from '@mantine/core'
-import Header from '../../components/Header'
-import NavBar from '../../components/NavBar'
-import Card from '../../components/MarketplaceCard'
+import { Grid } from '@mantine/core'
+import ProfileLayout from '../ProfileLayout'
+import MarketplaceCard from '@/app/components/MarketplaceCard'
 
 export default function Bids() {
-  let bid1 = Card({
+  let bid1 = MarketplaceCard({
     title: 'Ride Share Service',
     image: '/drive.jpg',
     description: 'Bid: $200 for 10 trips',
     id: 1,
   })
-  let bid2 = Card({
+  let bid2 = MarketplaceCard({
     title: 'Barber',
     image: '/barber.jpg',
     description: 'Bid: $400 for 10 haircuts',
@@ -20,18 +19,11 @@ export default function Bids() {
   })
 
   return (
-    <AppShell header={{ height: 120 }} navbar={{ width: 200, breakpoint: 'sm' }} padding="md">
-      {Header()}
-      {NavBar('/profile/bids')}
-      <AppShellMain>
-        <Title order={1} style={{ color: '#699B60', paddingLeft: 20, paddingTop: 20 }}>
-          Your Bids
-        </Title>
-        <Grid style={{ paddingLeft: 20, paddingTop: 20, paddingRight: 20 }}>
-          {bid1}
-          {bid2}
-        </Grid>
-      </AppShellMain>
-    </AppShell>
+    <ProfileLayout title="Your Bids" currentPath="/profile/bids">
+      <Grid>
+        {bid1}
+        {bid2}
+      </Grid>
+    </ProfileLayout>
   )
 }
